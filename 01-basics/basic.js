5e8a8597872b6f0dacf3d32a3724dc522e901125
Joao Pedro <joaopedro.dedepaula@gmail.com>
2017-07-27 15:36:57 -0300

    Change in basic.js

--- 01-basics/basic.js
+++ 01-basics/basic.js
@@ -2,12 +2,39 @@ console.log('Hi there! I am a JavaScript!')
 
 //ECMAScript 5
 var name = "Joao"
-console.log('==> My name is '+name)
-console.log('Type: '+typeof(name))
+console.log('==> My name is ' + name)
+console.log('Type: ' + typeof (name))
 
 //ECMAScript 6
 let numberA = 21
 let numberB = 33
-console.log('Number A: '+numberA)
-console.log('Number B: '+numberB)
-console.log('Type: '+typeof(numberA))
+console.log('Number A: ' + numberA)
+console.log('Number B: ' + numberB)
+console.log('Type: ' + typeof (numberA))
+
+let isThatTrue = true
+console.log('Is that true: ' + isThatTrue)
+console.log('Type: ' + typeof (isThatTrue))
+
+//Pure Function
+function adder(a, b) {
+    return a + b
+}
+
+let total = adder(numberA, numberB)
+
+//interpolação
+console.log(`${numberA} + ${numberB} = ${total}`)
+
+//Literal Object
+const person = {
+    firsName: 'Joao',
+    lastName: 'Pedro',
+    sayHello: function(){
+        console.log('Hi my name is ' + this.lastName)
+    }
+}
+
+console.log(`Mr. ${person.lastName}, ${person.firsName}`)
+person.sayHello()
+

36e244e5858979f4fc690580568ee1329835af09
Joao Pedro <joaopedro.dedepaula@gmail.com>
2017-07-27 14:21:56 -0300

    Added reference course source code

---
+++ 01-basics/basic.js
@@ -0,0 +1,13 @@
+console.log('Hi there! I am a JavaScript!')
+
+//ECMAScript 5
+var name = "Joao"
+console.log('==> My name is '+name)
+console.log('Type: '+typeof(name))
+
+//ECMAScript 6
+let numberA = 21
+let numberB = 33
+console.log('Number A: '+numberA)
+console.log('Number B: '+numberB)
+console.log('Type: '+typeof(numberA))
